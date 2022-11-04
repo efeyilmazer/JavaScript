4350ff8e11003135a471b0100156b29d1716e901
Efe Yılmazer <efekan1232@gmail.com>
2022-11-05 02:16:08 +0300

    updated

---
+++ Day 15/script.js
@@ -0,0 +1,44 @@
+            //Window objects
+
+let val;
+
+let a = 10;
+
+    function b(){
+        return 0;
+}
+
+
+
+//Alert
+alert("Merhaba");
+
+
+
+// //Prompt
+var c = prompt("Bir sayı giriniz");
+console.log(c);
+
+
+
+//Confirm
+var d = confirm("Emin misiniz?");
+if(d == true){
+     console.log("Tamam");
+}
+else {
+     console.log("İptal");
+}
+
+
+
+//Location
+val = window.location;
+val = window.location.href;
+val = window.location.hostname;
+val = window.location.host;
+val = window.location.protocol;
+
+
+
+console.log(val);
