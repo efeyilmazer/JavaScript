fdcd9ba51560a133a48555e4bd05759cf46f0699
Efe Yılmazer <efekan1232@gmail.com>
2022-12-01 23:37:05 +0300

    Create script.js

---
+++ Projects/button-ripple-effect/script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".ripple")
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) =>{
+        const x = e.clientX
+        const y = e.clientY
+    })
+})
