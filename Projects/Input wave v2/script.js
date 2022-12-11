b460fb645e848e919138f3a3dc5e2660b35187f8
Efe Yılmazer <efekan1232@gmail.com>
2022-12-11 23:33:50 +0300

    Create script.js

---
+++ Projects/Input wave v2/script.js
@@ -0,0 +1,9 @@
+const buttonOn = document.querySelector("button")
+const light = document.querySelector(".light")
+const name = document.querySelector("h1")
+
+function LightOn () {
+    if (light.classList.contains("active"))
+} else {
+
+}
