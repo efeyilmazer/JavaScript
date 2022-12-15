d0de72651ce19bb9b611d2f1855ecd8de0d65347
Efe Yılmazer <efekan1232@gmail.com>
2022-12-16 01:51:39 +0300

    Update script.js

--- Projects/Dad Joke Generator/script.js
+++ Projects/Dad Joke Generator/script.js
@@ -2,26 +2,20 @@ const jokeEl = document.querySelector("#joke")
 const jokeBtn = document.querySelector("#jokeBtn")
 
 jokeBtn.addEventListener("click", generateJoke)
-
 generateJoke()
 
 function generateJoke() {
   const config = {
     headers: {
-    Accept: "application/json",
+      Accept: "application/json",
     },
   }
-
-
-
-
   fetch("https://icanhazdadjoke.com", config)
   .then(res => res.json())
   .then(data => {
     jokeEl.innerHTML = data.joke
   })
 }
-
 // USING .then()
 // function generateJoke() {
 //   const config = {
