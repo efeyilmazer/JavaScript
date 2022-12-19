a0a7edbe9ee4021838c8cb711a03ffbdf555057d
Efe Yılmazer <efekan1232@gmail.com>
2022-12-19 03:38:59 +0300

    Delete script.js

--- Random picker/script.js
+++
@@ -1,7 +0,0 @@
-const tagsEl = document.querySelector("#tags")
-const textarea = document.querySelector("textarea")
-
-textarea.focus()
-textarea.addEventListener("keyup" , (e) => {
-    console(e.target.value)
-})
