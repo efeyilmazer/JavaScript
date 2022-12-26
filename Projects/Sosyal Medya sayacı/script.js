bd1fcefff58ee6e2af85a509a9e92b8051a5a956
Efe Yılmazer <efekan1232@gmail.com>
2022-12-26 05:20:25 +0300

    Update script.js

--- Projects/Sosyal Medya sayacı/script.js
+++ Projects/Sosyal Medya sayacı/script.js
@@ -1,22 +1,17 @@
 const counters = document.querySelectorAll(".counter")
 
 counters.forEach(counter => {
-    counter.innerText = "0"
+    counter.innerText = 0
 
     const updateCounter = () => {
-        const target = +counter.getAttribute("data-target")
         const c = +counter.innerText
+        const target = +counter.getAttribute("data-target")
 
-        const increment = target / 200
-
+        const increment = target / 100
         if(c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`
-            setTimeout(updateCounter(), 1)
-        }
-        else {
-            counter.innerText = target
+            setTimeout(updateCounter, 1)
         }
     }
-
     updateCounter()
 })
