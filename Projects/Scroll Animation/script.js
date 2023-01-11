dd64354a84d15268d052e90f4f2c56fc7ce923ac
Efe Yılmazer <efekan1232@gmail.com>
2023-01-11 22:08:26 +0300

    Create script.js

---
+++ Projects/Scroll Animation/script.js
@@ -0,0 +1,30 @@
+const boxes = document.querySelectorAll(".box")
+
+window.addEventListener("scroll", scroll)
+scroll()
+
+function scroll() {
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
+        if(boxTop < triggerBottom) {
+            box.classList.add("show")
+        } else {
+            box.classList.remove("show")
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
