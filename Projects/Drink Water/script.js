afb06c54d54b0948693249b2806377a633ca384b
Efe Yılmazer <84992900+efeyilmazer@users.noreply.github.com>
2023-01-31 23:01:09 +0300

    Update script.js

--- Projects/Drink Water/script.js
+++ Projects/Drink Water/script.js
@@ -1,4 +1,4 @@
-cccconst smallCups = document.querySelectorAll(".cup-small")
+ccconst smallCups = document.querySelectorAll(".cup-small")
 const liters = document.getElementById("liters")
 const percentage = document.getElementById("percentage")
 const remained = document.getElementById("remained")
