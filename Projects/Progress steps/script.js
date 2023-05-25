9bb696dfd12ab4b698a6b0e4e394852d99bb0c65
Efe Yılmazer <efekan1232@gmail.com>
2023-05-26 01:33:59 +0300

    Update script.js

--- Projects/Progress steps/script.js
+++ Projects/Progress steps/script.js
@@ -1,37 +1,39 @@
-const progress = document.querySelector("#progress")
+const progress = document.querySelector(".progress")
 const next = document.querySelector("#next")
 const prev = document.querySelector("#prev")
 const circles = document.querySelectorAll(".circle")
 
-let currentActive = 1;
+let currentActive = 1
 
 next.addEventListener("click", () => {
     currentActive++
-    if (currentActive > circles.length) {
+
+    if(currentActive > circles.length) {
         currentActive = circles.length
     }
-    update();
+    console.log(currentActive)
+    update()
 })
 
 prev.addEventListener("click", ()=> {
     currentActive--
+
     if(currentActive < 1) {
         currentActive = 1
     }
-    update();
+    console.log(currentActive)
+    update()
 })
 
 function update() {
-    circles.forEach((circle, value) => {
-        if (value < currentActive) {
+    circles.forEach((circle,value) => {
+        if(value < currentActive) {
             circle.classList.add("active")
         } else {
             circle.classList.remove("active")
         }
     })
-    const actives = document.querySelectorAll(".active")
-    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + "%"
-    if(currentActive === 1) {
+    if (currentActive === 1) {
         prev.disabled = true
     } else if (currentActive === circles.length) {
         next.disabled = true
@@ -39,4 +41,7 @@ function update() {
         prev.disabled = false
         next.disabled = false
     }
+    const actives = document.querySelectorAll(".active")
+    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + "%"
 }
+
