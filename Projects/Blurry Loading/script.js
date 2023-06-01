95d325c0a4109b9a0ca84f82de8ac1f0e2c0e2be
Efe Yılmazer <efekan1232@gmail.com>
2023-06-01 22:49:21 +0300

    Update script.js

--- Projects/Blurry Loading/script.js
+++ Projects/Blurry Loading/script.js
@@ -1,23 +1,20 @@
 const bg = document.querySelector(".bg")
-const loadText = document.querySelector(".loading-text")
+const loadText = document.querySelector (".loading-text")
 
-let load = 0
+let load = 0;
 
-let int = setInterval(blurring, 1)
+let int = setInterval(blurring, 10)
 
 function blurring() {
   load++
   if(load > 99) {
     clearInterval(int)
   }
-  loadText.innerText=`${load}%`
-  loadText.style.opacity= scale(load, 0, 100 ,1 ,0)
-  bg.style.filter=`blur(${scale(load, 0, 100, 30, 0)}px)`
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load,0,100,1,0)
+  bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
 }
 
-
-
-//Stackoverflow'dan destek aldım. (Görünürden görünmeze doğru gitmesini sağlıyor)
 const scale =(num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
