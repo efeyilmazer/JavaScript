b141e35e9b7c58a8d325313d888d4ae85b2afecd
Efe Yılmazer <efekan1232@gmail.com>
2023-06-01 22:49:16 +0300

    Update script.js

--- Projects/Input wave/script.js
+++ Projects/Input wave/script.js
@@ -3,7 +3,7 @@ const labels = document.querySelectorAll(".form-control label")
 labels.forEach(label => {
     label.innerHTML = label.innerText
     .split("")
-    .map((letters, idx) => `<span style="transition-delay:${idx * 50}ms">${letters}</span>`)
+    .map((letters, idx) => `<span style="transition-delay:${idx * 80}ms">${letters}</span>`)
     .join("")
 })
 
